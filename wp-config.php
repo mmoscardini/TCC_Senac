f8426be84a84ede29c12f4cd09baa4eb0270346d
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2017-09-09 16:25:13 -0300

    Produtos adicionados - cafeteiras e tvs

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,9 @@
  * @package WordPress
  */
 
+ //** Traduzindo woocommerce **//
+ define('WPLANG', 'pt_BR');
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'elder_db');
